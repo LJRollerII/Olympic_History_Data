2104359af97d66971d62730b33499a8c154a7d78
LJRollerII <ljcoolkid@optonline.net>
2022-11-01 14:29:49 -0400

    Answered Questions 1-7 and 11.

--- scripts/olympic_history.sql
+++ scripts/olympic_history.sql
@@ -15,12 +15,95 @@ FROM athlete_events
 
 --===================================================================================================================================================================--
 -- 2. List down all Olympics games held so far.
+SELECT DISTINCT year, season, city
+FROM athlete_events
+ORDER BY year
+
+-- Answer: List completed
+--===================================================================================================================================================================--
 -- 3. Mention the total no of nations who participated in each olympics game?
--- 4. Which year saw the highest and lowest no of countries participating in olympics?
--- 5. Which nation has participated in all of the olympic games?
+
+SELECT games, COUNT(DISTINCT noc) AS number_of_nations
+FROM athlete_events
+GROUP BY games
+ORDER BY games
+--Although this query looks good we may be missing some nations that are in the noc_regions table.
+
+-- Let's use a WITH statement to include that table as well to confirm that we didn't miss any nations.
+WITH all_countries AS
+        (SELECT games, nr.region
+        FROM athlete_events AS ae
+        JOIN noc_regions AS nr 
+		ON nr.noc = ae.noc
+        GROUP BY games, nr.region)
+SELECT games, COUNT(1) AS number_of_nations
+FROM all_countries
+GROUP BY games
+ORDER BY games
+    
+-- Results are pretty similar with some slight difference in the number of nations participating in certain games.
+
+-- Answer: Number of nations mentioned in above query
 
 --===================================================================================================================================================================--
+-- 4. Which year saw the highest and lowest no of countries participating in olympics?
 
+--Based of the query from question 3, 12 and 204 should be our minimum and maximum.
+-- Let's write a query just to get those numbers.
+
+ WITH all_countries AS
+              (SELECT games, nr.region
+              FROM athlete_events AS ae
+              JOIN noc_regions AS nr 
+			  ON nr.noc = ae.noc
+              GROUP BY games, nr.region),
+          tot_countries AS
+              (SELECT games, COUNT(1) AS total_countries
+              FROM all_countries
+              GROUP BY games)
+      SELECT DISTINCT
+      concat(FIRST_VALUE(games) OVER(ORDER BY total_countries)
+      , ' - ', FIRST_VALUE(total_countries) OVER(ORDER BY total_countries)) AS Lowest_Countries,
+      concat(FIRST_VALUE(games) OVER(ORDER BY total_countries DESC)
+      , ' - ', FIRST_VALUE(total_countries) over(order by total_countries DESC)) AS Highest_Countries
+      FROM tot_countries
+      ORDER BY 1;
+
+-- Answer: Lowest number of countires participating was 12 in 1896
+-- Answer: Highest number of countires participating was 204 in 2016
+
+--===================================================================================================================================================================--
+-- 5. Which nation has participated in all of the olympic games?
+
+--Based off the answer to Question 1. We know these nations need to have participated in 51 Olympic Games.
+
+-- To answer this we will need to:
+-- a. Find the total number olympic games (Refer to query in Question 1: 51)
+-- b. Find the total number of nations that have particpated in the olympics (Refer to all_countries definition in Qustion 4)
+-- c. Compare a & b
+
+WITH total_games AS 
+	  (SELECT COUNT(DISTINCT games) AS total_games
+	   FROM athlete_events),
+	 countries AS
+      (SELECT games, nr.region AS country
+       FROM athlete_events AS ae
+       JOIN noc_regions AS nr 
+	   ON nr.noc = ae.noc
+       GROUP BY games, country),
+      countries_participated AS 
+      (SELECT country, COUNT(1) AS total_participated_games
+       FROM countries
+       GROUP BY country)
+SELECT cp.*
+FROM countries_participated AS cp
+JOIN total_games AS tg
+ON tg.total_games = cp.total_participated_games
+ORDER BY 1
+
+-- Answer: France, Italy, Switzerland, and the United Kingdom (UK) were the only countries that participated in all Olympic Games.
+
+--===================================================================================================================================================================--
 -- 6. Identify the sport which was played in all summer olympics.
 
 -- To answer this we will need to:
@@ -50,8 +133,27 @@ ON t1.total_summer_games = t3.number_of_games
 --Answer: Swimming, Cycling, Fencing, Gymnastics, and Athletics (Track & Field) were played at all Summer Games.
 
 --===================================================================================================================================================================--
-
 -- 7. Which Sports were just played only once in the olympics?
+
+WITH t1 AS
+      (SELECT DISTINCT games, sport
+       FROM athlete_events),
+     t2 AS
+       (SELECT sport, COUNT(1) AS num_of_games
+        FROM t1
+        GROUP BY sport)
+SELECT t2.*, t1.games
+FROM t2
+JOIN t1 
+ON t1.sport = t2.sport
+WHERE t2.num_of_games = 1
+ORDER BY t1.sport;
+
+
+
+-- Answer: Basque Pelota, Cricket, Croquet, Jeu De Paume, Military Ski Patrol, Motorboating, Racquets, Roque, and Rugby Sevens were only played once at the Olympics.
+
+--===================================================================================================================================================================--
 -- 8. Fetch the total no of sports played in each olympic games.
 -- 9. Fetch details of the oldest athletes to win a gold medal.
 -- 10. Find the Ratio of male and female athletes participated in all olympic games.
@@ -162,6 +264,8 @@ CONCAT(FIRST_VALUE(country) OVER(PARTITION BY games ORDER BY  bronze DESC), ' - 
 	   FIRST_VALUE(bronze) OVER(PARTITION BY games ORDER BY bronze DESC)) AS Max_Bronze
 FROM TEMP
 ORDER BY games
+
+-- Answer: Countries indentified using query above
 --===================================================================================================================================================================--
 -- 17. Identify which country won the most gold, most silver, most bronze medals and the most medals in each olympic games.
 -- 18. Which countries have never won gold medal but have won silver/bronze medals?
